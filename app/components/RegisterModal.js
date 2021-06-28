574b2ee36f3f09dc286bd843a99ebaa4b912b74c
harry oh <harryoh3831@gmail.com>
2021-06-28 22:44:43 +0800

    register form input validation

--- app/components/RegisterModal.js
+++ app/components/RegisterModal.js
@@ -19,10 +19,9 @@ import * as ImagePicker from "expo-image-picker";
 
 import colors from "../config/colors";
 import RoAPI from "../api/restaurant-owner-caller";
+import { inputRegexValidation } from "../utilities/inputRegexes";
 
 function RegisterModal({ onPromptRegisterModal }) {
-  const [selectedLogo, setSelectedLogo] = useState(null);
-
   //input data update
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -32,6 +31,16 @@ function RegisterModal({ onPromptRegisterModal }) {
   const [contactNo, setContactNo] = useState("");
   const [restaurantName, setRestaurantName] = useState("");
   const [restaurantAddress, setRestaurantAddress] = useState("");
+  const [selectedLogo, setSelectedLogo] = useState(null);
+
+  //submission success signal
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  //input error signal
+  const [inputError, setInputError] = useState({
+    errorField: null,
+    errorMsg: null,
+  });
 
   const handleChooseLogo = async () => {
     const options = {
@@ -48,24 +57,108 @@ function RegisterModal({ onPromptRegisterModal }) {
     }
   };
 
-  const handleSubmitRegistration = async () => {
-    let data = {
-      username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      restaurant: {
-        restaurantName: restaurantName,
-        restaurantAddress: restaurantAddress,
-        logoFilePath: selectedLogo,
-      },
-      contactNo: contactNo,
-    };
+  const validateFormData = () => {
+    var validate = false;
 
-    const response = await RoAPI.registerRO(data);
-    if (response.success) {
-      console.log("submitted!");
+    if (username.trim() === "") {
+      setInputError({
+        errorField: "username",
+        errorMsg: "username cannot be emtpy.",
+      });
+    } else if (password.trim() === "") {
+      setInputError({
+        errorField: "password",
+        errorMsg: "password cannot be empty.",
+      });
+    } else if (lastName.trim() === "") {
+      setInputError({
+        errorField: "lastName",
+        errorMsg: "last name cannot be empty.",
+      });
+    } else if (email.trim() === "") {
+      setInputError({
+        errorField: "email",
+        errorMsg: "email cannot be empty.",
+      });
+    } else if (restaurantName.trim() === "") {
+      setInputError({
+        errorField: "restaurantName",
+        errorMsg: "business name cannot be empty.",
+      });
+    } else if (restaurantAddress.trim() === "") {
+      setInputError({
+        errorField: "restaurantAddress",
+        errorMsg: "restaurant address cannot be empty.",
+      });
+    } else if (contactNo.trim() === "") {
+      setInputError({
+        errorField: "contactNo",
+        errorMsg: "contact no. cannot be empty.",
+      });
+    } else if (!inputRegexValidation.usernameRegex.test(username)) {
+      setInputError({
+        errorField: "username",
+        errorMsg:
+          "username must has at least 5 characters and cannot contain special characters.",
+      });
+    } else if (!inputRegexValidation.passwordRegex.test(password)) {
+      setInputError({
+        errorField: "password",
+        errorMsg:
+          "password must has a minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character.",
+      });
+    } else if (!inputRegexValidation.firstNameRegex.test(firstName)) {
+      setInputError({
+        errorField: "firstName",
+        errorMsg: "first name cannot cotains special characters.",
+      });
+    } else if (!inputRegexValidation.lastNameRegex.test(lastName)) {
+      setInputError({
+        errorField: "lastName",
+        errorMsg: "last name cannot cotains special characters.",
+      });
+    } else if (!inputRegexValidation.emailRegex.test(email)) {
+      setInputError({ errorField: "email", errorMsg: "Invalid email format." });
+    } else if (restaurantAddress.length < 10) {
+      setInputError({
+        errorField: "restaurantAddress",
+        errorMsg: "restaurant address must has at least 10 characters.",
+      });
+    } else if (!inputRegexValidation.contactNoRegex.test(contactNo)) {
+      setInputError({
+        errorField: "contactNo",
+        errorMsg: "Invalid contact no.",
+      });
+    } else {
+      setInputError({ errorField: null, errorMsg: null });
+      validate = true;
+    }
+    return validate;
+  };
+
+  const handleSubmitRegistration = async () => {
+    const validateInput = validateFormData();
+
+    if (validateInput) {
+      let data = {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        restaurant: {
+          restaurantName: restaurantName,
+          restaurantAddress: restaurantAddress,
+          logoFilePath: selectedLogo,
+        },
+        contactNo: contactNo,
+      };
+
+      const response = await RoAPI.registerRO(data);
+      if (response.success) {
+        console.log("submitted!");
+        setSubmitSuccess(true);
+      }
     }
   };
 
@@ -86,6 +179,7 @@ function RegisterModal({ onPromptRegisterModal }) {
           <Layout style={styles.formInputContainer}>
             <Input
               onChangeText={(text) => setUsername(text)}
+              status={inputError.errorField === "username" && "danger"}
               textStyle={styles.formInput}
               placeholder="Username"
             ></Input>
@@ -93,6 +187,7 @@ function RegisterModal({ onPromptRegisterModal }) {
           <Layout style={styles.formInputContainer}>
             <Input
               onChangeText={(text) => setPassword(text)}
+              status={inputError.errorField === "password" && "danger"}
               textStyle={styles.formInput}
               placeholder="Password"
               secureTextEntry={true}
@@ -100,13 +195,15 @@ function RegisterModal({ onPromptRegisterModal }) {
           </Layout>
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "firstName" && "danger"}
               onChangeText={(text) => setFirstName(text)}
               textStyle={styles.formInput}
-              placeholder="First name"
+              placeholder="First name (optional)"
             ></Input>
           </Layout>
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "lastName" && "danger"}
               onChangeText={(text) => setLastName(text)}
               textStyle={styles.formInput}
               placeholder="Last name"
@@ -114,6 +211,7 @@ function RegisterModal({ onPromptRegisterModal }) {
           </Layout>
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "email" && "danger"}
               onChangeText={(text) => setEmail(text)}
               textStyle={styles.formInput}
               placeholder="E-mail"
@@ -122,6 +220,7 @@ function RegisterModal({ onPromptRegisterModal }) {
 
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "restaurantName" && "danger"}
               onChangeText={(text) => setRestaurantName(text)}
               textStyle={styles.formInput}
               placeholder="Business name"
@@ -130,6 +229,7 @@ function RegisterModal({ onPromptRegisterModal }) {
 
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "restaurantAddress" && "danger"}
               multiline={true}
               onChangeText={(text) => setRestaurantAddress(text)}
               textStyle={styles.formMultiLineInput}
@@ -138,12 +238,17 @@ function RegisterModal({ onPromptRegisterModal }) {
           </Layout>
           <Layout style={styles.formInputContainer}>
             <Input
+              status={inputError.errorField === "contactNo" && "danger"}
               onChangeText={(text) => setContactNo(text)}
               textStyle={styles.formInput}
               placeholder="Contact No."
               size="large"
             ></Input>
           </Layout>
+
+          {inputError.errorField && (
+            <Text style={styles.errorMsg}>{inputError.errorMsg}</Text>
+          )}
 
           <Layout style={styles.formInputContainer}>
             <Button size="large" status="info" onPress={handleChooseLogo}>
@@ -182,6 +287,14 @@ const styles = StyleSheet.create({
     height: 3,
     marginTop: 15,
     width: "80%",
+  },
+  errorMsg: {
+    alignSelf: "flex-start",
+    paddingLeft: 25,
+    paddingRight: 25,
+    color: colors.error,
+    fontSize: 15,
+    marginTop: 10,
   },
   formInput: {
     fontSize: 19,
