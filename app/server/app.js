37a53c43c8435910df894321a1eb693a742c08f5
harry oh <harryoh3831@gmail.com>
2021-07-11 20:47:03 +0800

    changes for host

--- app/server/app.js
+++ app/server/app.js
@@ -14,8 +14,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to node js");
 });
 
-app.listen(3000, () => {
-  console.log("server is ready at port 3000");
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+  console.log("server is ready at port " + PORT);
 });
 
 //import routes
